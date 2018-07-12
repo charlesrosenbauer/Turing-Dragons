5b6d08507f79cc6e0bef2804e2b038fe47ccb954
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-07-12 23:51:24 +0000

    Added rng functions to spacetable stuff

---
+++ SpaceAnalysis/src/util.c
@@ -0,0 +1,42 @@
+#include "util.h"
+
+
+
+
+
+
+
+
+
+
+uint8_t rnum(){
+  rstate = (rstate * 56137596173) + 135917635781;
+  return (rstate >> 8) & 0xFF;
+}
+
+
+
+
+
+
+
+
+
+
+uint64_t rnum8(){
+  rstate = (rstate * 3178956978153) + 65718936781;
+  uint64_t ret = (rstate >> 8) & 0xFFFF;
+  rstate = (rstate * 1537697165781) + 70873891123;
+  ret <<= 16;
+  ret |= (rstate >> 8) & 0xFFFF;
+
+  rstate = (rstate * 7895103781351) + 51347674897;
+  ret <<= 16;
+  ret |= (rstate >> 8) & 0xFFFF;
+
+  rstate = (rstate * 8071853978411) + 48169789611;
+  ret <<= 16;
+  ret |= (rstate >> 8) & 0xFFFF;
+
+  return ret;
+}
---
+++ SpaceAnalysis/src/util.h
@@ -0,0 +1,35 @@
+#ifndef __UTIL_HEADER__
+#define __UTIL_HEADER__
+
+
+#include "stdint.h"
+
+
+
+
+
+
+
+
+
+
+static uint64_t rstate = 517839065173895;
+
+
+
+
+
+
+
+
+
+
+uint8_t  rnum ();
+uint64_t rnum8();
+
+
+
+
+
+
+#endif
