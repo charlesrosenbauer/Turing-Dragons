adcde0c60492970da838269f2ec005fcb9f5b471
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2018-07-20 00:25:00 +0000

    Now generates and prints some space tables

--- SpaceAnalysis/src/main.c
+++ SpaceAnalysis/src/main.c
@@ -1,4 +1,5 @@
 #include "spacetable.h"
+#include "stdio.h"
 
 
 
@@ -10,5 +11,19 @@
 
 
 int main(){
-  
+  SPACETABLE_ISA* isa = generateTableISA();
+
+  printf("Generated\n");
+
+  for(int i = 0; i < isa->bs; i++){
+    printf("binop %i\n", i);
+    printTable_2P(&isa->binops[i]);
+  }
+
+  printf("\n\n");
+
+  for(int i = 0; i < isa->us; i++){
+    printf("unop %i\n", i);
+    printTable_1P(&isa->unops[i]);
+  }
 }
